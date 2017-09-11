fdca998ac93b24257764e473564d9359d88f7319
한진섭 <jshan@hanjinseob-ui-MacBook-Pro.local>
2017-09-12 00:33:04 +0900

    Initial Commit

---
+++ aaa/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  aaa
+//
+//  Created by 한진섭 on 2017. 9. 12..
+//  Copyright © 2017년 한진섭. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
